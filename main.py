e718dbf308155c726d22b6198e4ebb6b5a60748d
João Pedro Nimuendaju <joao.nimuendaju@souunit.com.br>
2020-12-06 19:56:50 -0300

    teste de "busca" interativo

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ filmes = pd.read_csv("dados/movies.csv", sep=",")
 ratings = pd.read_csv("dados/ratings.csv", sep=",")
 
 def grafoUser(ratings): #usuarios (filmes e nota) em lista de adjacencia
+    print("a buildar")
     uId = ratings["userId"]
     uFilmes = ratings["movieId"]
     uScore = ratings["rating"]
@@ -17,49 +18,39 @@ def grafoUser(ratings): #usuarios (filmes e nota) em lista de adjacencia
         #grafo[b].append(uFilmes[x]) #adicionar filmes e notas como inteiro
         grafo[b].append(uScore[x]) #nota como inteiro ou float
         x += 1
+    print("a mimir")
     return grafo
 
-def grafoFilmes(filmes,userFilmes,userId): #irei remover, foi so pra testar
+def grafoFilmes(filmes,ratings): #remover, foi so pra testar
+    userId = ratings["userId"]
+    userFilmes = ratings["movieId"]
     grafo = {}
     for i in filmes["movieId"]:  # montar o grafo adicionando vertices: filmes
         grafo[i] = []
     f = 0
-    for j in filmes[
-        "movieId"]:  # adicionar arestas "userId" aos filmes, usuarios que assitiram au filme x (isso leva uns 7 minutoskkkkkkkkkkkkkkkkk)
+    for j in filmes["movieId"]:  # adicionar arestas "userId" aos filmes, usuarios que assitiram au filme x (isso leva uns 7 minutoskkkkkkkkkkkkkkkkk)
         for k in userFilmes:
             if k == j:
                 grafo[j].append(userId[f])
             f += 1
         f = 0
+    print("\nPronto!")##
     return grafo
 
-def topFilmes(filmes,userFilmes,userId): #teste
-    grafo = grafoFilmes(filmes,userFilmes,userId)
-    numeros = []
-    top10 = []
-    print("terminou o grafo")
-    print(len(grafo[329]),len(grafo[316]),len(grafo[307]),len(grafo[2571]))
-    for x in grafo:
-        numeros.append(len(grafo[x]))
-        numeros.sort(reverse=True)
-        top10 = numeros[0:10]
-    print("d")
-    for o in range(9):
-        for c in grafo:
-            if len(grafo[c]) == numeros[o]:
-                top10.append(c)
-
-    print("terminou o vetor")
-    return top10
-
-
+################################################
 grafoU = grafoUser(ratings)
+print("grafo completo")
 
 usuario = int(input("Id do usuário [1 - 611]: "))
-print(f"Informações o usuário: {usuario}\n['filme',nota]\n{grafoU[usuario]}") #user 611 adicionado como teste ao arquivo
+print(f"Informações o usuário: {usuario} - {len(grafoU[usuario])//2} Filmes vistos. \n['filme',nota]\n{grafoU[usuario]}") #user 611 adicionado como teste ao arquivo
 
-print("top10")
+######### filme trava zap
+print("\naguenta um pouco q agr demora...")
+grafoF = grafoFilmes(filmes,ratings)
+filme = int(input("id do filme: "))
+print(f"Info do filme Id {filme} - Visto por: {len(grafoF[filme])} Usuarios")
 
-
-
+######### teste de zap, segundo run nao trava
+filme = int(input("id do filme: "))
+print(f"Info do filme Id {filme} - Visto por: {len(grafoF[filme])} Usuarios")
 #print(len(topFilmes(filmes,userFilmes,userId)))
