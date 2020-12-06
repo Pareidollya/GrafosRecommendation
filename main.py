024402278a0e2b5e6c2b0e360dd67e565f4c8757
João Pedro Nimuendaju <joao.nimuendaju@souunit.com.br>
2020-12-06 15:39:53 -0300

    top10 tosco, vou tirar dps

--- main.py
+++ main.py
@@ -3,12 +3,6 @@ import pandas as pd
 filmes = pd.read_csv("dados/movies.csv", sep=",")
 ratings = pd.read_csv("dados/ratings.csv", sep=",")
 
-print(ratings["userId"])
-
-userFilmes = ratings["movieId"]
-userId = ratings["userId"]
-print(userId[10])
-
 def grafoUser(ratings):
     grafo = {}
     for i in ratings["userId"]:
@@ -28,9 +22,40 @@ def grafoFilmes(filmes,userFilmes,userId):
             f += 1
         f = 0
     return grafo
+
+def topFilmes(filmes,userFilmes,userId): #teste
+    grafo = grafoFilmes(filmes,userFilmes,userId)
+    numeros = []
+    top10 = []
+    print("terminou o grafo")
+    print(len(grafo[329]),len(grafo[316]),len(grafo[307]),len(grafo[2571]))
+    for x in grafo:
+        numeros.append(len(grafo[x]))
+        numeros.sort(reverse=True)
+        top10 = numeros[0:10]
+    print("d")
+    for o in range(9):
+        for c in grafo:
+            if len(grafo[c]) == numeros[o]:
+                top10.append(c)
+
+    print("terminou o vetor")
+    return top10
+
+print(ratings["userId"])
+
+userFilmes = ratings["movieId"]
+userId = ratings["userId"]
+
 print("passou ")
 
 print("sun")
-print(grafoUser(ratings))
+
 print("\nnovo grafo ai\n")
-print(grafoFilmes(filmes,userFilmes,userId))
+#print(grafoFilmes(filmes,userFilmes,userId))
+
+print("top10")
+print(topFilmes(filmes,userFilmes,userId))
+
+
+#print(len(topFilmes(filmes,userFilmes,userId)))
