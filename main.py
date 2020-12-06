fceaf79e6708b41c53c85649ae3223c4de9aab59
João Pedro Nimuendaju <joao.nimuendaju@souunit.com.br>
2020-12-06 13:18:58 -0300

    defGrafos

--- main.py
+++ main.py
@@ -1,29 +1,35 @@
 import pandas as pd
 
-filmes = pd.read_csv("dados/movies.csv",sep=",")
-ratings = pd.read_csv("dados/ratings.csv",sep=",")
+filmes = pd.read_csv("dados/movies.csv", sep=",")
+ratings = pd.read_csv("dados/ratings.csv", sep=",")
 
 print(ratings["userId"])
 
-userFilmes= ratings["movieId"]
+userFilmes = ratings["movieId"]
 userId = ratings["userId"]
 print(userId[10])
 
-grafo = {}
-for i in filmes["movieId"]: #montar o grafo adicionando vertices: filmes 
-    grafo[i] = []
+def grafoUser(ratings):
+    grafo = {}
+    for i in ratings["userId"]:
+        grafo[i] = []
+    return grafo
+
+def grafoFilmes(filmes,userFilmes,userId):
+    grafo = {}
+    for i in filmes["movieId"]:  # montar o grafo adicionando vertices: filmes
+        grafo[i] = []
+    f = 0
+    for j in filmes[
+        "movieId"]:  # adicionar arestas "userId" aos filmes, usuarios que assitiram au filme x (isso leva uns 7 minutoskkkkkkkkkkkkkkkkk)
+        for k in userFilmes:
+            if k == j:
+                grafo[j].append(userId[f])
+            f += 1
+        f = 0
+    return grafo
 print("passou ")
 
-f = 0
-for j in filmes["movieId"]: #adicionar arestas "userId" aos filmes, usuarios que assitiram au filme x (isso leva uns 7 minutoskkkkkkkkkkkkkkkkk)
-    for k in userFilmes:
-        if k == j:
-            grafo[j].append(userId[f])
-        f += 1
-    f = 0
-    
 print("sun")
-print(grafo)
-
-
-
+print(grafoUser(ratings))
+print(grafoFilmes(filmes,userFilmes,userId))
