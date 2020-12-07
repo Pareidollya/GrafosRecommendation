e2d14e9e3409f5b47bceebea42967266cbc81e63
João Pedro Nimuendaju <joao.nimuendaju@souunit.com.br>
2020-12-06 23:54:37 -0300

    refatoração e Linked list em userGraph
    limpeza no codigo e aprimoramento do grafo

--- main.py
+++ main.py
@@ -3,20 +3,21 @@ import pandas as pd
 filmes = pd.read_csv("dados/movies.csv", sep=",")
 ratings = pd.read_csv("dados/ratings.csv", sep=",")
 
-def grafoUser(ratings): #usuarios (filmes e nota) em lista de adjacencia
+def grafoUser(ratings): #linked list
     print("a buildar")
     uId = ratings["userId"]
     uFilmes = ratings["movieId"]
     uScore = ratings["rating"]
     grafo = {}
+
     for a in ratings["userId"]: #criar vertices
         grafo[a] = []
     x = 0
-
     for b in uId: #adicionar adjacencia aos vertices: filmes, valor(nota)
-        grafo[b].append(f"{uFilmes[x]}") #adicionar filmes como String
-        #grafo[b].append(uFilmes[x]) #adicionar filmes e notas como inteiro
-        grafo[b].append(uScore[x]) #nota como inteiro ou float
+        l = []
+        l.append(uFilmes[x])
+        l.append(uScore[x])
+        grafo[b].append(l)
         x += 1
     print("a mimir")
     return grafo
@@ -39,18 +40,5 @@ def grafoFilmes(filmes,ratings): #remover, foi so pra testar
 
 ################################################
 grafoU = grafoUser(ratings)
-print("grafo completo")
 
-usuario = int(input("Id do usuário [1 - 611]: "))
-print(f"Informações o usuário: {usuario} - {len(grafoU[usuario])//2} Filmes vistos. \n['filme',nota]\n{grafoU[usuario]}") #user 611 adicionado como teste ao arquivo
 
-######### filme trava zap
-print("\naguenta um pouco q agr demora...")
-grafoF = grafoFilmes(filmes,ratings)
-filme = int(input("id do filme: "))
-print(f"Info do filme Id {filme} - Visto por: {len(grafoF[filme])} Usuarios")
-
-######### teste de zap, segundo run nao trava
-filme = int(input("id do filme: "))
-print(f"Info do filme Id {filme} - Visto por: {len(grafoF[filme])} Usuarios")
-#print(len(topFilmes(filmes,userFilmes,userId)))
